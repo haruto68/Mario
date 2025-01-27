1f26fba7d44b24e3a28c6468c19b639d9fa35c10
haruto68 <138412489+haruto68@users.noreply.github.com>
2025-01-27 09:40:09 +0900

    1.27

--- Mario/Mario/Objects/Terrain/Terrain.h
+++ Mario/Mario/Objects/Terrain/Terrain.h
@@ -3,14 +3,10 @@
 #include"../GameObject.h"
 #include"../GameObjectManager.h"
 
-class Character : public GameObject
+class Terrain : public GameObject
 {
 protected:
-	Vector2D velocity = 0.0f;
-	int hit_point = 0;
-	bool on_ground = true;
-
-	float g_velocity = 0.0;
+	
 
 public:
 	virtual void OnHitCollision(GameObject*) override
